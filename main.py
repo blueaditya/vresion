83289d3bff8be1efbadb1f6f2738fb3c54f42860
blueaditya <adityaneelabha190603@gmail.com>
2022-01-12 10:34:27 +0530

    Tkinter Application Fully Implimented.

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def move_folder():
         mb.showerror('Error', 'We could not move your folder')
 
 
-def list_file_in_folder():
+def list_files_in_folder():
 
     folder = fd.askdirectory(
         title='Select the folder whose files you want to list')
@@ -80,3 +80,43 @@ def rename_file():
 
     file = fd.askopenfilename(title='Choose a file to rename', filetypes=[
                               ("All files", "*.*")])
+
+
+# Initalizing Variable for Tkinter
+title = 'Blueaditya File Manager'
+background = 'Purple'
+
+button_font = ("Times New Roman", 13)
+button_background = "Turquoise"
+
+# Initalizing the Tkinter window
+
+
+root = Tk()
+root.title(title)
+root.geometry('250x400')
+root.resizable(0, 0)
+root.config(bg=background)
+
+# Creating and placing the components in the window
+Label(root, text=title, font=("Comic Sans MS", 15),
+      bg=background, wraplength=250).place(x=20, y=0)
+Button(root, text='Open a file', width=20, font=button_font,
+       bg=button_background, command=open_file).place(x=30, y=50)
+Button(root, text='Copy a file', width=20, font=button_font,
+       bg=button_background, command=copy_file).place(x=30, y=90)
+Button(root, text='Rename a file', width=20, font=button_font,
+       bg=button_background, command=rename_file).place(x=30, y=130)
+Button(root, text='Delete a file', width=20, font=button_font,
+       bg=button_background, command=delete_file).place(x=30, y=170)
+Button(root, text='Open a folder', width=20, font=button_font,
+       bg=button_background, command=open_folder).place(x=30, y=210)
+Button(root, text='Delete a folder', width=20, font=button_font,
+       bg=button_background, command=delete_folder).place(x=30, y=250)
+Button(root, text='Move a folder', width=20, font=button_font,
+       bg=button_background, command=move_folder).place(x=30, y=290)
+Button(root, text='List all files in a folder', width=20, font=button_font, bg=button_background,
+       command=list_files_in_folder).place(x=30, y=330)
+# Finalizing the window
+root.update()
+root.mainloop()
